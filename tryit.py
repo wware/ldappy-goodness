41680d3d048e40c25e6a424312b5c072b29a9793
Will Ware <wware@ruelala.com>
2013-07-29 13:27:40 -0400

    minor script tweaks

--- tryit.py
+++ tryit.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python
+
 import sys
 import ldap
 import pprint
 
-server, dn, password = sys.argv[1:4]
+try:
+	server, dn, password = sys.argv[1:4]
+except:
+	print 'Usage: %s <server> <username> <password>' % sys.argv[0]
+	sys.exit(0)
 
 l = ldap.initialize('ldap://' + server)
 l.set_option(ldap.OPT_REFERRALS, False)
 l.protocol_version = 3
 try:
     l.simple_bind_s(dn, password)
-    print 'Happy happy joy joy'
+    print 'Authentication successful'
 except ldap.INVALID_CREDENTIALS:
     print 'ZUT ALORS! The password she is wrong.'
     sys.exit(0)
@@ -17,7 +23,7 @@ except ldap.INVALID_CREDENTIALS:
 
 if 'search' in sys.argv[1:]:
     # Once the bind has succeeded, we can do a search.
-    # But a successful bind is sufficient for user authentication.
+    # But a successful bind should be sufficient for user authentication.
 
     # base = "dc=qa,dc=ruelala,dc=lan"
     base = ','.join(map(lambda x: 'dc=' + x, server.split('.')))
